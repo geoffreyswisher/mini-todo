076cb48b0971f70c9165dd63c358b6cb120e9d61
Geoffrey Swisher <geoffreydswisher@gmail.com>
2020-09-09 22:07:33 -0400

    minimum product

---
+++ app/Stack.py
@@ -0,0 +1,63 @@
+class Stack:
+
+	def __init__(self, stack=[], size=0):
+		self.stack = stack
+		self.size = size
+
+
+
+	def push(self, item):
+		self.stack.append(item)
+		self.size += 1
+		return True
+
+	def insert(self, place, item):
+		if place > self.size:
+			raise Exception('Index Error', '>')
+			return -1
+		if place <= 0:
+			raise Exception('Index Error', '<')
+			return -1
+
+		self.stack.insert(place, item)
+		self.size += 1
+		return True
+
+	def popLast(self):
+		return self.pop(self.size - 1)
+		
+	def popFirst(self):
+		return self.pop(0)
+
+	def pop(self, place):
+		if place >= self.size:
+			raise Exception('Index Error', '>')
+			return -1
+		if place < 0:
+			raise Exception('Index Error', '<')
+			return -1
+
+		val = self.stack.pop(place)
+		self.size -= 1
+		return val
+
+	def clear(self):
+		self.stack = []
+		self.size = 0
+
+
+	def toArray(self):
+		return self.stack
+
+	def getSize(self):
+		return self.size
+
+	def getItem(self, place):
+		return self.stack[place]
+
+	def getFirst(self):
+		return self.stack[0]
+
+	def getLast(self):
+		return self.stack[self.size - 1]
+
---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Warning: sudo will be used to create the command in /usr/bin/"
+echo "Nothing harmful will happen, you may just be prompted for a password"
+
+
+if command -v python >/dev/null 2>&1; then
+	:
+else
+	echo "python not installed, this installation will fail"
+	echo "please install python with 'sudo apt install python2.7' or preferred version"
+fi
+
+
+sudo cp bin/todo /usr/bin/
+
+name=$(whoami)
+cp -r ./ /home/$name/todolist/
